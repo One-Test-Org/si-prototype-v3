aa07edc5ae9d1593c5d7cfaed999c1653b4bc7fd
Nikolaos-Gkionis <nikolaos.gkionis@nhsbsa.nhs.uk>
2023-11-17 11:06:01 +0000

    address

--- app/views/download/routes.js
+++ app/views/download/routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
+// API
+const axios = require('axios');
 
 // Add your routes here - above the module.exports line
 
@@ -34,7 +36,7 @@ router.post('/address-question', function (req, res) {
     let addressTypeDec = req.session.data.addressTypeDec;
 
     if (addressTypeDec == "Yes") {
-        res.redirect('person-uk-address');
+        res.redirect('find-person-uk-address');
     } else {
         res.redirect('person-address');
     }
@@ -71,6 +73,56 @@ router.get('/file-download', function (req, res) {
     const filePath = path.join(__dirname, '/supplier.pdf')
     // Send the file
     res.download(filePath)
+
+})
+
+router.post('/select-person-uk-address', function (req, res) {
+    res.redirect('check-answers');
+});
+
+router.post('/find-person-uk-address', function (req, res) {
+
+    var postcodeLookup = req.session.data['postcode']
+
+    const regex = RegExp('^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$');
+
+    if (postcodeLookup) {
+
+        if (regex.test(postcodeLookup) === true) {
+
+            axios.get("https://api.os.uk/search/places/v1/postcode?postcode=" + postcodeLookup + "&key=" + "CS48P3ceaHollIQFsIMoP4oXLjvlbqp2")
+                .then(response => {
+                    var addresses = response.data.results.map(result => result.DPA.ADDRESS);
+
+                    const titleCaseAddresses = addresses.map(address => {
+                        const parts = address.split(', ');
+                        const formattedParts = parts.map((part, index) => {
+                            if (index === parts.length - 1) {
+                                // Preserve postcode (DL14 0DX) in uppercase
+                                return part.toUpperCase();
+                            }
+                            return part
+                                .split(' ')
+                                .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+                                .join(' ');
+                        });
+                        return formattedParts.join(', ');
+                    });
+
+                    req.session.data['addresses'] = titleCaseAddresses;
+
+                    res.redirect('select-person-uk-address')
+                })
+                .catch(error => {
+                    console.log(error);
+                    res.redirect('/download/person-uk-address')
+                });
+
+        }
+
+    } else {
+        res.redirect('/find-subject-uk-address')
+    }
 
 })
 
